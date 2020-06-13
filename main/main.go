239ca0de7ce40f65c4c9d6011ea9d99b68970639
ruipedroa <rui.p.alves96@gmail.com>
2020-06-13 22:52:48 +0100

    Collect CPU data
    Add code to collect CPU info from operating system.

--- main/main.go
+++ main/main.go
@@ -2,9 +2,19 @@ package main
 
 import (
 	"fmt"
+	"time"
+
+	"github.com/shirou/gopsutil/cpu"
 )
 
 func main() {
 
-	fmt.Println("Hello World!")
+	//Percent calculates the percentage of cpu used either per CPU or combined.
+	percent, _ := cpu_windows.Percent(time.Second, true)
+	fmt.Println("  User: %.2f\n", percent[cpu.CPUser])
+	fmt.Println("  Nice: %.2f\n", percent[cpu.CPNice])
+	fmt.Println("   Sys: %.2f\n", percent[cpu.CPSys])
+	fmt.Println("  Intr: %.2f\n", percent[cpu.CPIntr])
+	fmt.Println("  Idle: %.2f\n", percent[cpu.CPIdle])
+	fmt.Println("States: %.2f\n", percent[cpu.CPUStates])
 }
